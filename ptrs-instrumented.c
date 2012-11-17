e891ac0bfc5153acfbdfc6bc5668b79c6c350e50
httpd <httpd@vm-6858.localdomain>
2012-11-16 20:19:24 -0500

    instrumented ptr

---
+++ ptrs-instrumented.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void young_people();
+void old_people();
+
+void func();
+int main(int argc, char * argv[]) {
+  func();
+}
+
+void func() {
+  void (*handle)();
+  char user_input[1024];
+  char user_age[4];
+
+  printf("How old are you?\n");
+  fgets(user_age, 4, stdin);
+
+  // Set handler based on users age
+  int age = atoi(user_age);
+  if(age < 21) {
+    handle = young_people;
+  } else {
+    handle = old_people;
+  }
+
+  printf("%d Years old? What's your life story?\n", age);
+
+  printf("User input addr: %p", user_input);
+
+  // Read from terminal 
+  gets(user_input);
+
+  printf("Your life story is %s\n", user_input);
+
+  // Reply to user
+  handle();
+}
+
+void young_people() {
+  printf("Thanks for you life story, youngin.\n");
+}
+
+void old_people() {
+  printf("Thanks for your life story, old timer.\n");
+}
